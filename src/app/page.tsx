f0f8b66c919927597487238a535c5a3122fdffe9
oclix <manawaenziril@gmail.com>
2026-07-01 15:58:45 +0700

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import StrategyBuilder from "@/components/StrategyBuilder";
+import BotCard from "@/components/BotCard";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <StrategyBuilder />
+      <BotCard />
+    </>
+  );
+}
